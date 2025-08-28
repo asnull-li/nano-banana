84fa9df564879d3895bd3c315d92da8aa6e7cccb
asnull <fasv@qq.com>
2025-08-28 21:22:54 +0800

    调整NanoBanana每张图片的积分成本为4，并统一任务类型的引号格式

--- src/lib/constants/nano-banana.ts
+++ src/lib/constants/nano-banana.ts
@@ -2,7 +2,7 @@
 // 这个文件可以在客户端和服务器端使用
 
 // 每张图片消耗的积分数
-export const CREDITS_PER_IMAGE = 5;
+export const CREDITS_PER_IMAGE = 4;
 
 // 任务类型
-export type TaskType = 'text-to-image' | 'image-to-image';
+export type TaskType = "text-to-image" | "image-to-image";
